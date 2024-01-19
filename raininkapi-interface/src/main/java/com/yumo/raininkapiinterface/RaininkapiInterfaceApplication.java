903ebba2541cb2d6469106317a21ff459999816e
yumo <2120454642@qq.com>
2024-01-19 12:33:47 +0800

    add 新增模拟调用接口 简易sdk

---
+++ raininkapi-interface/src/main/java/com/yumo/raininkapiinterface/RaininkapiInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.yumo.raininkapiinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaininkapiInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaininkapiInterfaceApplication.class, args);
+    }
+
+}
